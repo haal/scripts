12e9b3df850f482df6293da4ed8a02c7053967f8
haris <alesevic@gmail.com>
2017-03-11 18:49:19 +0100

    Update

--- devtools_install.sh
+++ devtools_install.sh
@@ -15,23 +15,20 @@ endColor='\e[0m'
 TEMP_DIR="temp_install_data"
 if [ `uname -p` = "x86_64" ]; then
   #echo -e "${green} Preparing software for 64bit system ... ${endColor}"
-  MONGODB="http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.4.3.tgz"
-  SUBLIME_TEXT2="http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2"
-  JDK="http://download.oracle.com/otn-pub/java/jdk/7u21-b11/jdk-7u21-linux-x64.tar.gz"
-  GEMSET="cia"
-  SCALA="http://www.scala-lang.org/downloads/distrib/files/scala-2.10.1.tgz"
-  MAVEN3="http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz"
-  ECLIPSE="http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/juno/SR2/eclipse-java-juno-SR2-linux-gtk-x86_64.tar.gz"
+  MONGODB="https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.2.6.tgz"
+  SUBLIME_TEXT2="https://download.sublimetext.com/sublime_text_3_build_3114_x64.tar.bz2"
+  JDK="http://download.oracle.com/otn-pub/java/jdk/8u91-b14/jdk-8u91-linux-x64.tar.gz"
+  ECLIPSE="http://ftp.fau.de/eclipse/technology/epp/downloads/release/mars/2/eclipse-java-mars-2-linux-gtk-x86_64.tar.gz"
 else
   #echo -e "${green} Preparing software for 32bit system ... ${endColor}"
-  MONGODB="http://fastdl.mongodb.org/linux/mongodb-linux-i686-2.4.3.tgz"
+  MONGODB="https://fastdl.mongodb.org/linux/mongodb-linux-i686-3.2.6.tgz"
   SUBLIME_TEXT2="http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2"
-  JDK="http://download.oracle.com/otn-pub/java/jdk/7u21-b11/jdk-7u21-linux-i586.tar.gz"
-  GEMSET="cia"
-  SCALA="http://www.scala-lang.org/downloads/distrib/files/scala-2.10.1.tgz"
-  MAVEN3="http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz"
-  ECLIPSE="ftp://mirror.selfnet.de/eclipse/technology/epp/downloads/release/juno/SR2/eclipse-java-juno-SR2-linux-gtk.tar.gz"
+  JDK="http://download.oracle.com/otn-pub/java/jdk/8u91-b14/jdk-8u91-linux-i586.tar.gz"
+  ECLIPSE="https://eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/2/eclipse-java-mars-2-linux-gtk.tar.gz"
 fi
+GEMSET="cia"
+MAVEN3="http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
+SCALA="http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.tgz"
 RUBY_RAILS_DOC="http://railsapi.com/doc/rails-v3.2.6_ruby-v1.9.2/rdoc.zip"
 
 ###################
@@ -71,6 +68,7 @@ sublime_text() {
   ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/bin/sublime
 }
 
+#not up to date
 ruby_rails() {
   echo -e "${green} Installing Ruby and Rails... ${endColor}"
   curl -L get.rvm.io | bash -s stable
@@ -92,28 +90,28 @@ ruby_rails_docs() {
 }
 
 oracle_jdk() {
-  echo -e "${green} Installing Oracle JDK... ${endColor}"
+  echo -e "${green} Installing Oracle JDK... ${endColor}"export JAVA_HOME=/usr/lib/jvm/java-8-oracle
   cd ~/$TEMP_DIR
-  wget --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" $JDK
+  wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $JDK
   tar -xvf jdk-*
   sudo mkdir -p /usr/lib/jvm
-  sudo mv jdk1.7.0_* /usr/lib/jvm/jdk1.7.0
-  sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7.0/bin/java" 1
-  sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7.0/bin/javac" 1
-  sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.7.0/bin/javaws" 1
+  sudo mv jdk1.8.0_91* /usr/lib/jvm/jdk1.8.91
+  sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.91/bin/java" 1
+  sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.91/bin/javac" 1
+  sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.91/bin/javaws" 1
   sudo chmod a+x /usr/bin/java
   sudo chmod a+x /usr/bin/javac
   sudo chmod a+x /usr/bin/javaws
-  sudo chown -R root:root /usr/lib/jvm/jdk1.7.0
+  sudo chown -R root:root /usr/lib/jvm/jdk1.8.91
   #sudo update-alternatives --config java
   rm jdk-*
 }
 
 postgresql() {
   echo -e "${green} Installing PostgreSQL... ${endColor}"
-  sudo apt-get install -y postgresql-9.1 postgresql-client pgadmin3 libpq-dev
+  sudo apt-get install -y postgresql-9.5 postgresql-client pgadmin3 libpq-dev
 #  sudo -u postgres psql
-#  ALTER USER postgres PASSWORD 'newPassword';
+#  ALTER USER postgres PASSWORD 'newPasswrod';
 }
 
 heroku_toolbelt() {
@@ -135,14 +133,14 @@ scala() {
   cd $HOME/$TEMP_DIR
   wget $SCALA
   tar -xvf scala*
-  mv scala-2.10.1 /opt
-  ln -s /opt/scala-2.10.1/bin/scala /usr/bin/scala
-  ln -s /opt/scala-2.10.1/bin/scalac /usr/bin/scalac
-  ln -s /opt/scala-2.10.1/bin/fsc /usr/bin/fsc
-  ln -s /opt/scala-2.10.1/bin/sbaz /usr/bin/sbaz
-  ln -s /opt/scala-2.10.1/bin/sbaz-setup /usr/bin/sbaz-setup
-  ln -s /opt/scala-2.10.1/bin/scaladoc /usr/bin/scaladoc
-  ln -s /opt/scala-2.10.1/bin/scalap /usr/bin/scalap
+  sudo mv scala-2.11.8 /opt
+  sudo ln -s /opt/scala-2.11.8/bin/scala /usr/bin/scala
+  sudo ln -s /opt/scala-2.11.8/bin/scalac /usr/bin/scalac
+  sudo ln -s /opt/scala-2.11.8/bin/fsc /usr/bin/fsc
+  sudo ln -s /opt/scala-2.11.8/bin/sbaz /usr/bin/sbaz
+  sudo ln -s /opt/scala-2.11.8/bin/sbaz-setup /usr/bin/sbaz-setup
+  sudo ln -s /opt/scala-2.11.8/bin/scaladoc /usr/bin/scaladoc
+  sudo ln -s /opt/scala-2.11.8/bin/scalap /usr/bin/scalap
   rm scala*
 # remove
 # rm -rf /opt/scala /usr/bin/scala /usr/bin/scalac /usr/bin/fsc /usr/bin/sbaz /usr/bin/sbaz-setup /usr/bin/scaladoc /usr/bin/scalap
@@ -152,10 +150,10 @@ maven() {
   echo -e "${green} Installing Maven3 ... ${endColor}"
   cd $HOME/$TEMP_DIR
   wget $MAVEN3
-  tar -xvf apache-*
+  tar -xvf apache-maven*
   rm apache-maven*.tar.gz
-  mv apache-maven* /opt
-  ln -s /opt/apache-maven-3.0.5/bin/mvn /usr/bin/mvn
+  sudo mv apache-maven* /opt
+  sudo ln -s /opt/apache-maven-3.3.9/bin/mvn /usr/bin/mvn
 }
 
 eclipse() {
@@ -164,7 +162,8 @@ eclipse() {
   wget $ECLIPSE
   tar -xvf eclipse-java*
   mv eclipse ~/apps/
-  rm eclipse-juno*.tar.gz
+  mv ~/apps/eclipse ~/apps/eclipse-java
+  rm eclipse-java*.tar.gz
 }
 
 # selection of tools
